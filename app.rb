3b824cf7ae2a7eedda9b3ab3cbff6e1d29d7d0f7
shirley Valeton <shirley.valeton88@gmail.com>
2024-10-15 20:46:30 +0200

    modif

--- app.rb
+++ app.rb
@@ -5,3 +5,18 @@ require_relative 'lib/game' #les deux lignes require/relative servent à faire a
 require_relative 'lib/player'
 
 binding.pry #sert à faire les tests
+
+player1 = Player.new("she") #nouveau joueur
+player2 = Player.new("José")
+
+puts "Voici l'état de nos joueurs :"
+player1.show_state
+player2.show_state
+
+while player1.life_points > 0 && player2.life_points > 0 #repete tant que points 
+    #de vie joueur1 superieur à 0 et points de vie joueur2 sup. à 0
+    player1.attacks(player2) #joueur 1 attaque joueur2
+    break if player2.life_points <= 0 #stop si point de vie joueur2 inferieur
+    #ou egal à 0
+    player2.attacks(player1) #joueur2 attaque joueur1
+  end
--- lib/player.rb
+++ lib/player.rb
@@ -6,3 +6,25 @@ class player
     @life_points = 10 #points de vie du joueur = 10
     end
 end
+
+def show_state #methode pour initialise l'etat des joueurs
+    puts "#{@name} a #{@life_points} points de vie" #methode pour points de vie restante
+end
+
+def gets_damage(damage_received) #methode pour dommage reçus
+    @life_points -= damage_received #points de vies - dommage recus
+    if @life_points <= 0 #si les points de vies sont inferieur ou = à 0
+      puts "Le joueur #{@name} a été tué !"
+    end
+  end
+
+  def attacks(player) #methode pour attaquer un autre joueur
+    puts "#{@name} attaque #{player.name}"
+    damage = compute_damage #dommage = calcul les dommages entre 1 et 6
+    puts "Il lui inflige #{damage} points de dommages"
+    player.gets_damage(damage)
+  end
+  
+  def compute_damage #affecte compute damage à entre 1 et 6
+    rand(1..6)
+  end
